919bd68eb43b6b4487d342cb0f27a2fd5c9a20bf
Yupi <youngperchik2404@gmail.com>
2026-05-07 11:33:00 +0500

    add global styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neo: {
+          bg: '#FFFFFF',
+          dark: '#000000',
+          accent: '#A3FF00',
+          surface: '#F2F2F2',
+          danger: '#FF3131',
+          warning: '#FFB800',
+        }
+      },
+      boxShadow: {
+        'neo': '4px 4px 0px 0px rgba(0,0,0,1)',
+        'neo-hover': '2px 2px 0px 0px rgba(0,0,0,1)',
+        'neo-active': '0px 0px 0px 0px rgba(0,0,0,1)',
+      },
+      borderWidth: {
+        '3': '3px',
+      }
+    },
   },
   plugins: [],
 }
